eb57ba5ced778b65338252306664d7416da3f9ec
SkySol <mail@xsystem.one>
2025-04-14 00:01:27 +0530

    migrate to netxjs backend remove old project code

--- backend/routes/criminals.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const criminalsController = require('../controllers/criminalsController');
-
-router.get('/', criminalsController.getCriminals);
-router.post('/', criminalsController.addCriminal);
-
-module.exports = router;
