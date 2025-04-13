4c0f5f35726211cf51da115fd8f47cfbf58620ca
SkySol <mail@xsystem.one>
2025-04-14 00:10:14 +0530

    chore: update PostCSS config and add Biome configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   plugins: [
+    'postcss-flexbugs-fixes',
     'postcss-preset-env',
+    'postcss-custom-media',
+    '@csstools/postcss-global-data',
     'tailwindcss',
     'autoprefixer',
   ],
