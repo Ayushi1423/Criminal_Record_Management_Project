8a8011037fb733b8ac94713da97f0a46ab47196f
akshattanwar1 <akshat21042004@gmail.com>
2025-04-15 21:33:56 +0530

    urlupdate

--- src/app/resources/config.ts
+++ src/app/resources/config.ts
@@ -1,4 +1,4 @@
-export const baseURL = "http://localhost:3000";
+export const baseURL = "criminal-record-management-project.vercel.app";
 
 // default customization applied to the HTML in the main layout.tsx
 export const style = {
